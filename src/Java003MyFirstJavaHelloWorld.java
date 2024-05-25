29f7f573b19728e7950479b51eb04dc790a4bbac
Jonathan Mulu <yonathanmulu@gmai.com>
2024-05-25 19:54:34 +0300

    my first exercise on java

--- src/Java003MyFirstJavaHelloWorld.java
+++ src/Java003MyFirstJavaHelloWorld.java
@@ -1,5 +1,19 @@
+import java.net.SocketOption;
+import java.util.Scanner;
+
+//public class Java003MyFirstJavaHelloWorld {
+//    public static void main (String [] args) {
+//        System.out.println("Hello Hora Resort");
+//    }
+//}
 public class Java003MyFirstJavaHelloWorld {
-    public static void main (String [] args) {
-        System.out.println("Hello Hora Resort");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter an your favorite integer:  ");
+        int favoriteNumber = input.nextInt();
+
+        System.out.println(favoriteNumber + " it is my favorite integer too!");
+
     }
 }
