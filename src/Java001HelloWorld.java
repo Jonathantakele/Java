fc3f620c27591d5ef3ff3f30af7bd6edf3986f27
Jonathan Mulu <yonathanmulu@gmai.com>
2024-05-25 12:54:53 +0300

    third commit

--- src/Java001HelloWorld.java
+++ src/Java001HelloWorld.java
@@ -1,5 +1,5 @@
 public class Java001HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is the second test to hello world terminal");
+        System.out.println("This is the second test to hello world terminal test check ");
     }
 }
