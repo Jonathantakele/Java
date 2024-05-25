15df706d1dc6b5b252a9b25cc213a64bba1d9632
Jonathan Mulu <yonathanmulu@gmai.com>
2024-05-25 13:11:21 +0300

    the fifth commit

--- src/Java001HelloWorld.java
+++ src/Java001HelloWorld.java
@@ -2,5 +2,6 @@ public class Java001HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is the second test to hello world terminal test check ");
         System.out.println("This is the fourth test commit of the first day");
+        System.out.println("this is the fifth test commit of the first day");
     }
 }
